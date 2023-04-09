24d421ccdef7e72a930827269008f7dfd5eb9010
Juan Leo <juanleodev@gmail.com>
2023-04-09 12:20:29 +0200

    Enemy spawn system added

---
+++ Assets/Systems/EnemySpawner/EnemyWaveSO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Components.Enemies.Scripts;
+using UnityEngine;
+
+namespace Systems.EnemySpawner {
+    [CreateAssetMenu(fileName = "EnemyWaveSO", menuName = "ScriptableObjects/EnemyWave", order = 0)]
+    public class EnemyWaveSO : ScriptableObject {
+        public int delay = 30; //seconds
+
+        public List<EnemySO> enemyList;
+        public List<int> enemyAmount;
+
+        public List<EnemyInWave> getWaves() {
+            return enemyList.Select((enemyType, index) => new EnemyInWave(enemyType, enemyAmount[index])).ToList();
+        }
+    }
+}
